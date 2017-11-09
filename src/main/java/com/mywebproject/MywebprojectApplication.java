26e145fbbd75646e0feb7e9f12339c9009e79007
santhosh2sai <ssanthosh9492@gmail.com>
2017-11-09 14:01:24 -0800

    added i18n

--- src/main/java/com/mywebproject/MywebprojectApplication.java
+++ src/main/java/com/mywebproject/MywebprojectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MywebprojectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MywebprojectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MywebprojectApplication.class, args);
+    }
 }
--- src/test/java/com/mywebproject/MywebprojectApplicationTests.java
+++ src/test/java/com/mywebproject/MywebprojectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import org.springframework.beans.factory.annotation.Autowired;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MywebprojectApplicationTests {
@@ -12,5 +12,6 @@ public class MywebprojectApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+
 
 }
